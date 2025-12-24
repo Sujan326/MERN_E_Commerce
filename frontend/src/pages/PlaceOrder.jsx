262daf3ba93d5cb1b1cca4e4769452f95138d0ea
Sujan <sujanramesh91766@gmail.com>
2025-12-24 22:16:28 +0530

    Completed frontend folder and routing setup

---
+++ frontend/src/pages/PlaceOrder.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PlaceOrder() {
+  return (
+    <div>
+      Place order
+    </div>
+  )
+}
+
+export default PlaceOrder
---
+++ readme.md
@@ -0,0 +1,3 @@
+FRONTEND:
+
+1. [TAILWIND CONFIGURATION](https://tailwindcss.com/docs/installation/using-vite)
