77417e60c938dc5810333de32c7a44aac78b86f5
Sujan <sujanramesh91766@gmail.com>
2025-12-25 11:28:52 +0530

    Completed Lastest and Best Seller section for frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import BestSeller from '../components/BestSeller'
 import Hero from '../components/Hero'
+import LatestCollection from '../components/LatestCollection'
 
 function Home() {
   return (
     <div>
       <Hero />
+      <LatestCollection />
+      <BestSeller />
     </div>
   )
 }
